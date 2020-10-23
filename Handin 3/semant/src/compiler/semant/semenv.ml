3914df84aa51028e96671a705cc3682614719ef9
Vitus Bjørn <201808133@post.au.dk>
2020-10-23 16:24:31 +0200

    Added baseTenv

--- Handin 3/semant/src/compiler/semant/semenv.ml
+++ Handin 3/semant/src/compiler/semant/semenv.ml
@@ -12,8 +12,8 @@ type enventry
   | FunEntry of { formals: Types.ty list; result: Types.ty }
 
 
-(* Should contain base INT and String *)  
-let baseTenv = S.empty (* TODO *)
+(* Should contain base INT and STRING *)  
+let baseTenv = S.enter (S.enter( S.empty, S.symbol "string", Ty.STRING), S.symbol "int", Ty.INT)
 
 (* Should contain tiger standard library - see Appel p.519 *)
 let baseVenv = S.empty(* TODO *)
